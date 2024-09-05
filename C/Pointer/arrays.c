5e2dfd8df1bf6447843c892fe39f9ced394deae6
o.le <oleschulz123@gmail.com>
2024-09-05 13:05:29 +0200

    Adding more basics

---
+++ C/Pointer/arrays.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("A array is just a pointer to the first element of the declared array."
+            "That mean you can access an element with these two methods:\n\n");
+    
+    /*
+     * Important: When you declare an array like this all elements but the first
+     * element might be NOT 0! (Those values are the old values that were writen
+     * into this address before declaring the array).
+     */
+    int arr[5];
+    printf("Access the first element with normal array arithmetic: %d\n", arr[0]);
+    printf("Access first element with pointer arithmetic: %d\n\n", *(arr + 0));
+
+    /*
+     * If you declare your array like this these values will be set to the right
+     * index. See comment above.
+     */
+    printf("You can also set a pointer that will point to the array\n");
+    int arrTwo[] = {4, 2, 6, 9};
+    int *arrPtr = arrTwo;
+
+    printf("With the help of pointer arithmetic you can iterate through the array");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Value of index %d: %d\n", i, *arrPtr++);
+    }
+}
+
