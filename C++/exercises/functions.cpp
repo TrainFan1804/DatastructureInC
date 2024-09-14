b8ebe4a01caf764ede6344567fffd094129a3d61
o.le <oleschulz123@gmail.com>
2024-09-14 14:34:03 +0200

    Adding reference return type

--- C++/exercises/functions.cpp
+++ C++/exercises/functions.cpp
@@ -1,11 +1,24 @@
 /*
  * Unlike C you should NOT use pointers for arguments but references instead
  */
+
 int add(const int &numOne, const int &numTwo);
+
+/*
+ * When you want to return a reference the returned reference must be a constant
+ * value. You can not return a variable which was initialise in the local scope of
+ * the function.
+ */
+const int &max(const int &first, const int &second)
+{
+    return (first > second) ? first : second;
+}
 
 int main()
 {
     int sum = add(5, 10);
+
+    int m = max(80, 50);
 }
 
 /*
