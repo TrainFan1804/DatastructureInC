7e088b8e6090f422af1e87d07d2bc3f173fb2635
o.le <oleschulz123@gmail.com>
2024-09-15 10:02:49 +0200

    Adding sizeof operator

--- C++/3. Datatypes/misc.cpp
+++ C++/3. Datatypes/misc.cpp
@@ -12,5 +12,8 @@ int main()
     constexpr int eVar = 22;
     std::cout << "This variable was set during compile time: " << eVar << "\n";
 
+    std::cout << "The size of an int: " << sizeof(int) << "\n";
+    std::cout << "The size of an long: " << sizeof(long) << "\n";
+
     return 0;
 }
